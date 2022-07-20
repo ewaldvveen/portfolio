5e0b3242ae3087c1063752894b64714b50d9eeef
Ewald van Veen <ewaldvanveen@gmail.com>
2022-07-20 13:42:16 +0200

    Removed some boilerplate code

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/c598cd4c-7fd1-4630-8d48-0da7dc7b3a97/deploy-status)](https://app.netlify.com/sites/evv-js-boilerplate/deploys)
-# js-boilerplate
-My personal JavaScript development environment.
+# Portfolio
+Ewald van Veen | Personal portfolio website.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 // Dummy code to check if everything is working from the start.
 import './index.css';
-import numeral from 'numeral';
 import { deleteUser, getUsers } from './api/userApi';
 
-const boilerplateValue = numeral(1000).format('$0,0.00');
-
-console.log(
-  `I would pay ${boilerplateValue} for this awesome JavaScript boilerplate!`
-);
 
 // Populate a table of users via API call
 getUsers().then((result) => {
