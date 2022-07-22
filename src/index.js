cfba705a8ecb7d5f2b86b650f1e7a101156be663
Ewald van Veen <ewaldvanveen@gmail.com>
2022-07-22 16:13:25 +0200

    Start creating a site structure using CSS Grid

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 // Dummy code to check if everything is working from the start.
 import './index.scss';
-import { deleteUser, getUsers } from './api/userApi';
+//import { deleteUser, getUsers } from './api/userApi';
 
 
 // Populate a table of users via API call
-getUsers().then((result) => {
-  let usersBody = '';
+// getUsers().then((result) => {
+//   let usersBody = '';
 
-  result.forEach((user) => {
-    usersBody += `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.id}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.email}</td>
-      </tr>`;
-  });
+//   result.forEach((user) => {
+//     usersBody += `<tr>
+//       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+//       <td>${user.id}</td>
+//       <td>${user.firstName}</td>
+//       <td>${user.lastName}</td>
+//       <td>${user.email}</td>
+//       </tr>`;
+//   });
 
-  global.document.getElementById('users').innerHTML = usersBody;
+//   global.document.getElementById('users').innerHTML = usersBody;
 
-  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+//   const deleteLinks = global.document.getElementsByClassName('deleteUser');
 
-  // Must use 'Array.from' to create a real array from a DOM-collection.
-  // 'getElementsByClassName' only creates an 'array like' object.
-  Array.from(deleteLinks, link => {
-    link.onclick = function(event) {
-      const element = event.target;
-      event.preventDefault();
+//   // Must use 'Array.from' to create a real array from a DOM-collection.
+//   // 'getElementsByClassName' only creates an 'array like' object.
+//   Array.from(deleteLinks, link => {
+//     link.onclick = function(event) {
+//       const element = event.target;
+//       event.preventDefault();
 
-      deleteUser(element.attributes['data-id'].value);
+//       deleteUser(element.attributes['data-id'].value);
 
-      const row = element.parentNode.parentNode;
-      row.parentNode.removeChild(row);
-    }
-  });
-});
+//       const row = element.parentNode.parentNode;
+//       row.parentNode.removeChild(row);
+//     }
+//   });
+// });
